f29e5b7eee057cc052ef18609a185b346434f7c7
JuancaJCJ <camilojcj@gmail.com>
2020-08-13 21:30:58 -0500

    Cors indexado

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const PORT = process.env.PORT;
 const router = require("../routes");
 const fileUpload = require("express-fileupload")
+const cors = require('cors');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +11,8 @@ app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: '/tmp/'
 }))
+
+app.use(cors())
 app.use("/api/v1", router);
 
 module.exports = { app, PORT };
